b4354fbd923ced167d9d09287e0ef1d5a987f55b
vivekop0 <sarkarjii534@gamil.com>
2024-01-26 20:18:58 +0530

    26 jan update

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,24 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  function add(){
+    setCount(count+1)
+  }
+  function remove(){
+    if(count>0)
+  setCount(count-1)
+  }
+
 
   return (
     <>
-    <button onClick={()=>setCount(count => count +1)} >count is {count}</button>
+    <button onClick={add} >count is {count}</button>
+
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <button onClick={remove}>remove</button>
     
     </>
   )
